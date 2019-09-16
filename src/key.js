045e0961896e6b56055ee43f5c021dd69540b3b1
Donny Cheng <donccl@gmail.com>
2019-09-16 14:38:42 -0400

    speical keys now have a different background color

--- src/key.js
+++ src/key.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import './index.css';
+import States from './States';
+import StateHelper from './StateHelper';
 
 class Key extends React.Component {
     
     // constructor(props) {}
 
     render(){
-        const style = (this.props.keyValue==="0")? "key key-zero": "key";
+        let style;
+        if (this.props.keyValue==="0")  style = "key key-zero";
+        else if (this.props.keyValue==="+"
+            || this.props.keyValue==="-"
+            || this.props.keyValue==="="
+            || this.props.keyValue==="\u00f7"
+            || this.props.keyValue==="\u00d7")  style = "key special-key";
+        else style = "key";
+
         return(
             <button 
                 value={this.props.keyValue} 
